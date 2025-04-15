3819b3036b91a382d9400a78ab2c87782c9dcf7b
dwightrs70 <54112859+dwightrs70@users.noreply.github.com>
2025-04-15 13:16:21 +0000

    Update settings.py.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -71,17 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'octofit_db',
-        'HOST': 'localhost',
-        'PORT': 27017,
-    },
-}
 
 
 # Enable CORS
